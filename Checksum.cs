af15a6946f279a9fbf25a8d60a34059043269c9b
Daniel Stamer-Squair <uaine.teine@hotmail.com>
2023-12-26 11:33:56 +1100

    checksum type built for use in IO

--- Checksum.cs
+++ Checksum.cs
@@ -1,11 +1,39 @@
-﻿using Uaine.Objects.Primitives;
+﻿using System.Security.Cryptography;
+using Uaine.Objects.Primitives;
 
 namespace Uaine.IO.Checksum
 {
     public class Checksum : HashCode
     {
-        public Checksum(int keyValue) : base(keyValue)
+        public int Algorthim { get; }
+
+        public Checksum(byte[] tocheck) : base(CalculateSHA256Checksum(tocheck).HashBytes)
         {
+            Algorthim = ChecksumType.SHA256;
+        }
+        public Checksum(byte[] alreadychecksummed, int algorthimUsed) : base(alreadychecksummed)
+        {
+            Algorthim = algorthimUsed;
+        }
+
+        // Calculate checksum for a given file using MD5
+        public static HashCode CalculateMD5Checksum(byte[] data)
+        {
+            using (var md5 = MD5.Create())
+            {
+                byte[] checksumBytes = md5.ComputeHash(data);
+                return new HashCode(checksumBytes);
+            }
+        }
+
+        // Calculate checksum for byte array using SHA256
+        public static HashCode CalculateSHA256Checksum(byte[] data)
+        {
+            using (var sha256 = SHA256.Create())
+            {
+                byte[] checksumBytes = sha256.ComputeHash(data);
+                return new HashCode(checksumBytes);
+            }
         }
     }
 }
---
+++ ChecksumType.cs
@@ -0,0 +1,8 @@
+﻿namespace Uaine.IO.Checksum
+{
+    public static class ChecksumType
+    {
+        public const int SHA256 = 0;
+        public const int MD5 = 1;
+    }
+}
